f71abaf21f0d842ae23f097215567f5c4d40f2ac
TaddyMason1 <taddymason69420@gmail.com>
2024-12-09 04:55:29 +0700

    removed old api key.

--- src/config/wagmiconfig.ts
+++ src/config/wagmiconfig.ts
@@ -4,7 +4,7 @@ import { http } from "wagmi";
 
 export const config = getDefaultConfig({
   appName: "RFLEKTION",
-  projectId: process.env.REOWN_ID || "e79e46c63d3230119def1d367797711d",
+  projectId: REOWN_ID,
   chains: [arbitrumSepolia],
   transports: {
     [arbitrumSepolia.id]: http(),
